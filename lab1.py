98a83bde75bd6fee20982db86f618012b969368d
Sophia Ruduke <sruduke@ualberta.ca>
2023-09-13 18:20:59 -0600

    raw URL change

--- lab1.py
+++ lab1.py
@@ -1,6 +1,6 @@
 import requests
 
 #print(requests.__version__)
-
-response = requests.get("http://google.com/")
+#response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/sruduke/cmput404-lab1/master/lab1.py")
 print(response.text)
