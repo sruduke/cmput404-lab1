14f6622051834ea99e597d6bdea57b5b69c3034b
Sophia Ruduke <sruduke@ualberta.ca>
2023-09-20 00:50:04 -0600

    Overall script cleanup

--- lab1.py
+++ lab1.py
@@ -1,6 +1,17 @@
+# CMPUT 404 Virtualenv & cURL Lab
+# Sophia Ruduke
+
 import requests
 
-#print(requests.__version__)
-#response = requests.get("http://google.com/")
+div = "-"*20 + "\n"
+
+# virtualenv - 3
+print("Requests Version", requests.__version__, div, sep="\n")
+
+# curl - 5
+response = requests.get("http://google.com/")
+print("Google Request", response.text, div, sep="\n")
+
+# curl - 10
 response = requests.get("https://raw.githubusercontent.com/sruduke/cmput404-lab1/master/lab1.py")
-print(response.text)
+print("Source Code", response.text, div, sep="\n")
